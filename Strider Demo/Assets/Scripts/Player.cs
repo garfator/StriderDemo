6365bb6ba80a6c191d9484eaeace70785f4ad9bb
garfator <johnrvanwie@gmail.com>
2017-10-24 15:37:01 -0700

    Adding Project, needs more work for SC. Have to move away from Unity Collab
    Adding project

---
+++ Strider Demo/Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float maxSpeed = 3;
+    public float Speed = 50f;
+    public float jumpPower = 150f;
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetBool("Grounded", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+
+        if(Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if(Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+        }
+
+
+
+	}
+
+
+     void FixedUpdate()
+    {
+
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+        easeVelocity.x *= 0.75f;
+
+        float h = Input.GetAxis("Horizontal");
+
+
+
+        if(grounded == true)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+
+
+        rb2d.AddForce((Vector2.right * Speed) * h);
+
+
+
+        if(rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if(rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+
+    }
+}
